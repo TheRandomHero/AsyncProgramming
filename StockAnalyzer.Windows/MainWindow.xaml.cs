6e9386c6944330e36b4919084f3570fdf9085b27
Attila Molnar <scythianhawk@gmail.com>
2020-07-04 13:04:08 +0200

    read from file

--- StockAnalyzer.Windows/MainWindow.xaml.cs
+++ StockAnalyzer.Windows/MainWindow.xaml.cs
@@ -1,13 +1,19 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
 using System.Net;
-using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Navigation;
 using Newtonsoft.Json;
 using StockAnalyzer.Core.Domain;
+using StockAnalyzer.Windows.Services;
 
 namespace StockAnalyzer.Windows
 {
@@ -18,43 +24,73 @@ namespace StockAnalyzer.Windows
             InitializeComponent();
         }
 
-        private async void Search_Click(object sender, RoutedEventArgs e)
+        CancellationTokenSource cancellationTokenSource = null;
+
+        private void Search_Click(object sender, RoutedEventArgs e)
         {
             #region Before loading stock data
             var watch = new Stopwatch();
             watch.Start();
             StockProgress.Visibility = Visibility.Visible;
             StockProgress.IsIndeterminate = true;
+
+            Search.Content = "Cancel";
             #endregion
 
-            using (var client = new HttpClient())
+            var lines = File.ReadAllLines(@"StockPrices_Small.csv");
+
+            var data = new List<StockPrice>();
+
+            foreach (var line in lines.Skip(1))
             {
-                var response = await client.GetAsync($"http://localhost:61363/api/stocks/{Ticker.Text}");
+                var segments = line.Split(',');
 
-                try
+                for (var i = 0; i < segments.Length; i++) segments[i] = segments[i].Trim('\'', '"');
+                var price = new StockPrice
                 {
-                    response.EnsureSuccessStatusCode();
-
-                    var content = await response.Content.ReadAsStringAsync();
-
-                    var data = JsonConvert.DeserializeObject<IEnumerable<StockPrice>>(content);
-
-                    Stocks.ItemsSource = data;
-                }
-                catch (Exception ex)
-                {
-
-                    Notes.Text += ex.Message;
-                }
-
-
+                    Ticker = segments[0],
+                    TradeDate = DateTime.ParseExact(segments[1], "M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture),
+                    Volume = Convert.ToInt32(segments[6], CultureInfo.InvariantCulture),
+                    Change = Convert.ToDecimal(segments[7], CultureInfo.InvariantCulture),
+                    ChangePercent = Convert.ToDecimal(segments[8], CultureInfo.InvariantCulture),
+                };
+                data.Add(price);
             }
-            
+
+            Stocks.ItemsSource = data.Where(price => price.Ticker == Ticker.Text);
+
             #region After stock data is loaded
             StocksStatus.Text = $"Loaded stocks for {Ticker.Text} in {watch.ElapsedMilliseconds}ms";
             StockProgress.Visibility = Visibility.Hidden;
+            Search.Content = "Search";
             #endregion
 
+            cancellationTokenSource = null;
+        }
+
+        private Task<List<string>> SearchForStocks(CancellationToken cancellationToken)
+        {
+            var loadLinesTask = Task.Run(async () =>
+            {
+                var lines = new List<string>();
+
+                using (var stream = new StreamReader(File.OpenRead(@"StockPrices_small.csv")))
+                {
+                    string line;
+                    while ((line = await stream.ReadLineAsync()) != null)
+                    {
+                        if (cancellationToken.IsCancellationRequested)
+                        {
+                            return lines;
+                        }
+                        lines.Add(line);
+                    }
+                }
+
+                return lines;
+            }, cancellationToken);
+
+            return loadLinesTask;
         }
 
         private void Hyperlink_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
